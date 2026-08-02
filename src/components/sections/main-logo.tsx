ecbfb3408ca70514162bdefad44346e2ae3afac2
reyeschristiandave582-debug <reyeschristiandave582@gmail.com>
2026-08-02 21:57:19 +0800

    Update main-logo.tsx

--- src/components/sections/main-logo.tsx
+++ src/components/sections/main-logo.tsx
@@ -6,11 +6,11 @@ const MainLogo = () => {
   return (
     <div 
       className="w-full flex justify-center pt-2 pb-0 cursor-pointer"
-      onClick={() => window.parent.postMessage({ type: "OPEN_EXTERNAL_URL", data: { url: "https://giftclick.org/aff_c?offer_id=163&aff_id=183864&source=Sephora" } }, "*")}
+      onClick={() => window.parent.postMessage({ type: "OPEN_EXTERNAL_URL", data: { url: "https://giftclick.org/aff_c?offer_id=440&aff_id=183864&source=Zara" } }, "*")}
     >
       <img 
         src="https://i.imgur.com/lfmzARL.png" 
-        alt="Sephora Logo" 
+        alt="Zara Logo" 
         className="h-10 sm:h-15 w-20 object-contain transition-all duration-700 hover:brightness-120"
       />
     </div>
