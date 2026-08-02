a70d99cc81bfc90c31dafaf36de6fb1e420b2ade
reyeschristiandave582-debug <reyeschristiandave582@gmail.com>
2026-08-02 22:41:39 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,20 +5,20 @@ import ErrorReporter from "@/components/ErrorReporter";
 import Script from "next/script";
 
 export const metadata: Metadata = {
-  title: "Sephora Gift Card",
-  description: "Participate in the Panera Bread Rewards program and get a chance to claim a $100 gift card.",
+  title: "Zara Gift Card",
+  description: "Participate in the Zara program and get a chance to claim a $750 gift card.",
     icons: {
     icon: [
-      { url: "https://i.imgur.com/6jvUviD.png", type: "image/png" },
+      { url: "https://i.imgur.com/VpoTDAq.png", type: "image/png" },
     ],
-    shortcut: "https://i.imgur.com/6jvUviD.png",
+    shortcut: "https://i.imgur.com/VpoTDAq.png",
     apple: [
-      { url: "https://i.imgur.com/6jvUviD.png", sizes: "180x180", type: "image/png" },
+      { url: "https://i.imgur.com/VpoTDAq.png", sizes: "180x180", type: "image/png" },
     ],
     other: [
       {
         rel: "apple-touch-icon-precomposed",
-        url: "https://i.imgur.com/6jvUviD.png",
+        url: "https://i.imgur.com/VpoTDAq.png",
       },
     ],
   },
