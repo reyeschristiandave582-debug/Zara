974cc758f741f80b494cd0cbfc7a28db9043209f
reyeschristiandave582-debug <reyeschristiandave582@gmail.com>
2026-08-02 22:43:21 +0800

    Update main-logo.tsx

--- src/components/sections/main-logo.tsx
+++ src/components/sections/main-logo.tsx
@@ -9,7 +9,7 @@ const MainLogo = () => {
       onClick={() => window.parent.postMessage({ type: "OPEN_EXTERNAL_URL", data: { url: "https://giftclick.org/aff_c?offer_id=440&aff_id=183864&source=Zara" } }, "*")}
     >
       <img 
-        src="https://i.imgur.com/lfmzARL.png" 
+        src="https://i.imgur.com/4vwUTNN.png" 
         alt="Zara Logo" 
         className="h-10 sm:h-15 w-20 object-contain transition-all duration-700 hover:brightness-120"
       />
