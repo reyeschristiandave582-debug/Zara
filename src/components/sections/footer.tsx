2830b9f750dc4d397f95b6c7ef4c2df1dbd156a6
reyeschristiandave582-debug <reyeschristiandave582@gmail.com>
2026-08-02 22:01:59 +0800

    Update footer.tsx

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
       {/* Social Media Icons */}
       <div className="flex items-center justify-center gap-1.5 mb-2">
           <a 
-            href="https://www.facebook.com/sephora/" 
+            href="https://www.facebook.com/Zara/" 
             target="_blank"
             rel="noopener noreferrer"
             className="w-7 h-7 rounded-full bg-[#000001] hover:bg-[#000001] flex items-center justify-center text-white hover:scale-110 transition-transform duration-300"
@@ -18,7 +18,7 @@ const Footer = () => {
             <Facebook className="w-3.5 h-3.5 fill-current" />
           </a>
           <a 
-            href="https://www.instagram.com/sephora/?hl=en"
+            href="https://www.instagram.com/zara/?hl=en"
             target="_blank"
             rel="noopener noreferrer"
             className="w-7 h-7 rounded-full bg-[#000001] hover:bg-[#000001] flex items-center justify-center text-white hover:scale-110 transition-transform duration-300"
@@ -27,7 +27,7 @@ const Footer = () => {
             <Instagram className="w-3.5 h-3.5" />
           </a>
           <a 
-            href="https://www.linkedin.com/company/sephora" 
+            href="https://www.linkedin.com/company/zara" 
             target="_blank"
             rel="noopener noreferrer"
             className="w-7 h-7 rounded-full bg-[#000001] hover:bg-[#000001] flex items-center justify-center text-white hover:scale-110 transition-transform duration-300"
